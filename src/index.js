94f7c3ea2ff307405d22688fa11df73b27a42337
Kjerand Pedersen <kjerp@statoil.com>
2019-05-22 14:25:03 +0200

    0.2.1-alpha.0

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import * as vector from './vector';
-import * as matrix from './matrix';
-import * as functions from './functions';
-import * as constants from './constants';
 
-export default {
-  ...vector,
-  ...matrix,
-  ...functions,
-  ...constants,
-};
+// import Array2d from './array-2d';
+// import * as vector from './vector';
+// import * as matrix from './matrix';
+// import * as functions from './functions';
+// import * as constants from './constants';
+
+// export default {
+//   Array2d,
+//   ...vector,
+//   ...matrix,
+//   ...functions,
+//   ...constants,
+// };
 
 export { default as Array2d } from './array-2d';
+export * from './vector';
+export * from './matrix';
+export * from './functions';
+export * from './constants';
+
